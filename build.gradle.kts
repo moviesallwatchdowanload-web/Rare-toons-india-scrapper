5628352f22bbf01ea521860e49a1fadebff31e5d
Xeonmovies.github.io <moviesallwatchdowanload@gmail.com>
2026-09-17 13:21:17 +0530

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+// Top-level build file
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
