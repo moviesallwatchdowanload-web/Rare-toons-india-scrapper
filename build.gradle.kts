8bd6eed905d6c5c0f05ade9f5e856ea31597d6c8
Xeonmovies.github.io <moviesallwatchdowanload@gmail.com>
2026-09-17 14:12:52 +0530

    updated build.grade.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,25 @@
+// Top-level build file
 buildscript {
-        repositories {
-                    google()
-                            mavenCentral()
-                                    maven("https://jitpack.io")
-        }
-            dependencies {
-                        classpath("com.android.tools.build:gradle:8.1.4")
-                                classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-                                        // CloudStream Gradle plugin
-                                                classpath("com.lagradost:cloudstream3:pre-release")
+            repositories {
+                        google()
+                                mavenCentral()
+                                        maven("https://jitpack.io")
             }
+                dependencies {
+                                classpath("com.android.tools.build:gradle:8.1.4")
+                                        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+                                                classpath("com.lagradost:cloudstream3:pre-release")
+                }
 }
 
 allprojects {
-        repositories {
-                    google()
-                            mavenCentral()
-                                    maven("https://jitpack.io")
-        }
+            repositories {
+                        google()
+                                mavenCentral()
+                                        maven("https://jitpack.io")
+            }
 }
 
 tasks.register<Delete>("clean") {
-        delete(rootProject.buildDir)
+            delete(rootProject.buildDir)
 }
