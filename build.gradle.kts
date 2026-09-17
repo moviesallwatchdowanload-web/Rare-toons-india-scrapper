7c790cb8fd7285434931d109dc7d01c7a4146972
Xeonmovies.github.io <moviesallwatchdowanload@gmail.com>
2026-09-17 14:55:41 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
-import com.android.build.api.dsl.LibraryExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
+// Top-level build file
 buildscript {
     repositories {
         google()
@@ -24,60 +20,6 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
-    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(configuration: LibraryExtension.() -> Unit) =
-    extensions.getByName<LibraryExtension>("android").configuration()
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    android {
-        namespace = "com.megix"
-        compileSdk = 34
-        defaultConfig {
-            minSdk = 21
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-    }
-
-    tasks.withType<KotlinJvmCompile>().configureEach {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-            freeCompilerArgs.addAll(
-                listOf(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
-                )
-            )
-        }
-    }
-
-    dependencies {
-        val implementation by configurations
-        val cloudstream by configurations
-
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.18")
-        implementation("org.jsoup:jsoup:1.17.2")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-        implementation("com.squareup.okhttp3:okhttp:4.12.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
-        implementation("org.mozilla:rhino:1.7.14")
-        implementation("androidx.annotation:annotation:1.7.0")
-        implementation("androidx.browser:browser:1.8.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    }
-}
-
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    delete(rootProject.buildDir)
 }
